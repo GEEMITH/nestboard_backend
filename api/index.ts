2cfd51235940bb2897705911154d543d6fac55d0
A.U.M.D. Perera <jeromeperera93@gmail.com>
2026-08-19 00:34:27 +0530

    sum er update

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { buildApp } from "../src/app.js";
+
+const app = buildApp();
+
+export default function handler(
+  req: VercelRequest,
+  res: VercelResponse,
+) {
+  return app(req, res);
+}
